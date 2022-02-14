6773dcc3ee92a9c4a6edb075f7b0c64267841b09
Victor Chirino <vicchirino@gmail.com>
2022-02-14 16:55:26 -0300

    Categories

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Victor Chirino on 11/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .featured
+   
+    enum Tab {
+        case featured
+        case list
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
